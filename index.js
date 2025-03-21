81effdd7f1df993ddf48d3699d7dc217fcc1d384
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-03-21 14:29:54 +0530

    contact class

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
         this.zip = zip;
-        this.phone = phone;
+        this.phoneNumber = phoneNumber;
         this.email = email;
     }
 
-    toString() {
-        return `${this.firstName} ${this.lastName}, ${this.address}, ${this.city}, ${this.state} - ${this.zip}, Phone: ${this.phone}, Email: ${this.email}`;
+    display() {
+        console.log(`Name: ${this.firstName} ${this.lastName}`);
+        console.log(`Address: ${this.address}, ${this.city}, ${this.state} - ${this.zip}`);
+        console.log(`Phone: ${this.phoneNumber}`);
+        console.log(`Email: ${this.email}`);
     }
 }
