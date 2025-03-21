a97f9def1419281c41bd0394c4591bf720924177
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-03-21 14:44:21 +0530

    validation

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
 class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phone, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email = email;
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = this.validateName(firstName, "First Name");
+        this.lastName = this.validateName(lastName, "Last Name");
+        this.address = this.validateMinLength(address, "Address", 4);
+        this.city = this.validateMinLength(city, "City", 4);
+        this.state = this.validateMinLength(state, "State", 4);
+        this.zip = this.validateZip(zip);
+        this.phoneNumber = this.validatePhone(phoneNumber);
+        this.email = this.validateEmail(email);
     }
 
-    toString() {
-        return `${this.firstName} ${this.lastName}, ${this.address}, ${this.city}, ${this.state} - ${this.zip}, Phone: ${this.phone}, Email: ${this.email}`;
-    }
-    validateName(name) {
-        return /^[A-Z][a-zA-Z]{2,}$/.test(name);
+    validateName(name, fieldName) {
+        const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        if (!nameRegex.test(name)) {
+            throw new Error(`${fieldName} must start with a capital letter and have at least 3 characters.`);
+        }
+        return name;
     }
 
-    validateAddress(value) {
-        return value.length >= 4;
+    validateMinLength(value, fieldName, minLength) {
+        if (value.length < minLength) {
+            throw new Error(`${fieldName} must have at least ${minLength} characters.`);
+        }
+        return value;
     }
 
     validateZip(zip) {
-        return /^\d{6}$/.test(zip);
+        const zipRegex = /^[1-9][0-9]{5}$/;
+        if (!zipRegex.test(zip)) {
+            throw new Error("Zip code must be a valid 6-digit number.");
+        }
+        return zip;
     }
 
-    validatePhone(phone) {
-        return /^[6-9]\d{9}$/.test(phone);
+    validatePhone(phoneNumber) {
+        const phoneRegex = /^[0-9]{10}$/;
+        if (!phoneRegex.test(phoneNumber)) {
+            throw new Error("Phone number must be a valid 10-digit number.");
+        }
+        return phoneNumber;
     }
 
     validateEmail(email) {
-        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            throw new Error("Invalid email format.");
+        }
+        return email;
+    }
+
+    display() {
+        console.log(`Name: ${this.firstName} ${this.lastName}`);
+        console.log(`Address: ${this.address}, ${this.city}, ${this.state} - ${this.zip}`);
+        console.log(`Phone: ${this.phoneNumber}`);
+        console.log(`Email: ${this.email}`);
     }
 }
