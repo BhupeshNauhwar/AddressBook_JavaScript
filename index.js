9a5e8ab1ff85518a304be24a300538c552b823e6
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-03-22 11:47:07 +0530

    added

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function contactDetails(firstName,lastName,address,state,city,zipCode,email,phon
 }
 
 //UC3 
-contactDetails('Abhishek', 'Sagar', 'Home', 'Uttar Pradesh', 'Mathura', "281001", 'Abhishek@gmail.com', "9876543210");
+contactDetails('Bhupesh', 'kumar', 'Home', 'Uttar Pradesh', 'Mathura', "281001", 'bhupesh@gmail.com', "9876543210");
 console.log(addressBook);
 
 //UC4
