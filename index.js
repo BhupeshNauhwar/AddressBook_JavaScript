1e144fb39dd726d26cde28e4ca2d295cda04aae4
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-03-22 10:37:52 +0530

    Added

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
+// UC-1
+class Contact{
+    firstName;
+    lastName;
+    address;
+    state;
+    city;
+    zipCode;
+    email;
+    phoneNumber;
 
-    display() {
-        console.log(`Name: ${this.firstName} ${this.lastName}`);
-        console.log(`Address: ${this.address}, ${this.city}, ${this.state} - ${this.zip}`);
-        console.log(`Phone: ${this.phoneNumber}`);
-        console.log(`Email: ${this.email}`);
+   //created constructor
+    constructor(firstName,lastName,address,state,city,zipCode,email,phoneNumber){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.address = address
+        this.state = state
+        this.city = city
+        this.zipCode = zipCode
+        this.email = email
+        this.phoneNumber = phoneNumber
     }
 }
